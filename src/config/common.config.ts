87475b44bfa5ed5825fa7b9ac72829421964c38b
Pawel Krzyzak <pawel.krzyzak@epcnetwork.io>
2024-11-07 12:22:37 +0100

    fix common config

--- src/config/common.config.ts
+++ src/config/common.config.ts
@@ -2,6 +2,6 @@ export default {
   frontUrl: process.env.FRONT_URL
     ? process.env.FRONT_URL.split(',')
     : ['http://localhost:3000'],
-  port: process.env.PORT || 3000,
+  port: +(process.env.PORT || 3000),
   env: process.env.NODE_ENV || 'development',
 };
